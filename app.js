43e49cd0e02490c5a9462a0f04f911427d9d119d
Brandon Walden <brandonshanewalden@gmail.com>
2017-08-15 07:57:35 -0400

    Added runner form, still working on details page

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const db = require('./db')
 const mustacheExpress = require('mustache-express');
-
+const bodyParser = require('body-parser');
 const app = express()
 
-app.use(express.static('public'))
-
 app.engine('mustache', mustacheExpress());
-
 app.set('view engine', 'mustache');
 app.set('views', __dirname + '/views');
+app.use(express.static('public'))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
 app.get('/', (req, res, next) => {
   db.query('SELECT * FROM runner', [], (err, results) => {
@@ -26,9 +28,42 @@ app.get('/', (req, res, next) => {
 // 2. Show all runners not as json.
 // 3. Add a button to go to a details page. Select that looks up by bib_id.
 // 4. Make a form to add a new runner (division, sponsor, name).
-// 5.
 
+app.get('/add', (req, res) => {
+  res.render('addRunner')
+});
 
+app.post('/add', (req, res, next) => {
+  let addRunner =
+    `INSERT INTO runner(
+    division,
+    sponsor,
+    name
+  )
+  VALUES(
+    '${req.body.division}',
+    '${req.body.sponsor}',
+    '${req.body.name}'
+  )`;
+  db.query(addRunner, (err) => {
+    if (err) {
+      return next(err)
+    }
+    res.redirect('/')
+  });
+});
+
+app.get('/:id', (req, res, next) => {
+  const id = req.params.id
+  db.query(`SELECT * FROM runner WHERE bib_id = ${id}`, (err, results) => {
+    if (err) {
+      return next(err)
+    }
+    res.render('details', {
+      runner: results.rows
+    })
+  });
+});
 
 app.listen(3000, () => {
   console.log('Running races on port 3000...');
